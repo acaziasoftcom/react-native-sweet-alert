fb59fdf48ba091dc2703c5ca5f26bcaa9102894b
Quan Pham <doraemonfanclub@gmail.com>
2018-10-14 14:07:52 +0700

    Simplified APIs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * @providesModule RNSweetAlert
  * @flow
  */
+
 'use strict';
 
 import React, { NativeModules } from 'react-native';
@@ -9,40 +10,21 @@ import isHex from 'is-hexcolor';
 
 const Native = NativeModules.RNSweetAlert;
 
+const DEFAULT_OPTIONS = {
+  title: '',
+  subTitle: '',
+  confirmButtonTitle: 'OK',
+  confirmButtonColor: '#000',
+  otherButtonTitle: 'Cancel',
+  otherButtonColor: '#dedede',
+  style: 'success'
+}
+
 const SweetAlert = {
-  showAlert: (data = {}, acceptCallback = () => true, cancelCallback = () => true) => {
-    Native.showSweetAlert({
-      type: 'normal',
-      title: '',
-      contentText: '',
-      cancellable: true,
-      ...data
-    }, acceptCallback, cancelCallback);
+  showAlertWithOptions: (options = DEFAULT_OPTIONS, callback = () => {}) => {
+    Native.showAlertWithOptions(options, callback)
   },
   dismissAlert: () => Native.hideSweetAlert(),
-  changeAlertType: (type) => Native.changeAlertType(type),
-  showCancelButton: (bool = true) => {
-     if (typeof bool == 'boolean') {
-        Native.showCancelButton(bool);
-     }
-  },
-  showContentText: (bool = true) => {
-     if (typeof bool == 'boolean') {
-        Native.showContentText(bool);
-     }
-  },
-  setBarColor: (barColor) => {
-  	if (isHex(barColor)) {
-  		  Native.setBarColor(barColor);
-  	}
-  },
-  resetCount: () => {
-    Native.resetCount();
-  },
-  spin: () => {
-    Native.spin();
-  }
-
 };
 
 export default SweetAlert;
